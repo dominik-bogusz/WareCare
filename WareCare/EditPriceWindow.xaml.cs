d4ca231f725a2a35800984735ce30ebf9f3ffa03
Dominik-Bogusz <dominik.bogusz1111@gmail.com>
2022-09-01 18:37:10 +0200

    Completing the logic in the EditPriceWindow file

--- WareCare/EditPriceWindow.xaml.cs
+++ WareCare/EditPriceWindow.xaml.cs
@@ -28,10 +28,33 @@ namespace WareCare
             InitializeComponent();
         }
 
-
         private void Submit_Click(object sender, RoutedEventArgs e)
         {
-            
+            if (Application.Current.Windows.OfType<MainWindow>().Any())
+            {
+                foreach (MainWindow mainWindow in Application.Current.Windows.OfType<MainWindow>()) // get the MainWindow object
+                {
+                    using (WareCareContext db = new WareCareContext(MainWindow.connectionString))
+                    {
+                        Product selectedProduct = (Product)mainWindow.dgridAvailability.SelectedItem;
+                        var availability = (from a in db.AvailableProducts where a.ProductID == selectedProduct.ID select a).FirstOrDefault();
+                        try
+                        {
+                            if (!string.IsNullOrEmpty(tbxPrice.Text) && Decimal.Parse(tbxPrice.Text) >= 0) // if not empty proceed
+                                availability.Price = Decimal.Parse(tbxPrice.Text);
+                            else availability.Price = 0; // else make price 0
+
+                            db.SaveChanges();
+                            mainWindow.dgridAvailability.Items.Refresh();
+                            this.Close();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Invalid price format.");
+                        }
+                    }
+                }
+            }
         }
 
         private void Cancel_Click(object sender, RoutedEventArgs e)
@@ -39,14 +62,25 @@ namespace WareCare
             this.Close();
         }
 
+        private Boolean TextAllowed(String s) // regex; only numbers & control keys allowed
+        {
+            foreach (Char c in s.ToCharArray())
+            {
+                if (Char.IsNumber(c) || c == ',' || Char.IsControl(c)) continue; // change ',' to '.' in accordance to the system's language respectively
+                else return false;
+            }
+            return true;
+        }
+
         private void PreviewTextInputHandler(Object sender, TextCompositionEventArgs e)
         {
-
+            e.Handled = !TextAllowed(e.Text);
         }
 
         private void PastingHandler(object sender, DataObjectPastingEventArgs e)
         {
-            
+            String s = (String)e.DataObject.GetData(typeof(int));
+            if (!TextAllowed(s)) e.CancelCommand();
         }
     }
 }
