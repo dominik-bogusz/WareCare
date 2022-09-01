057bdcd36530514f82f5b11fac6dd28bb1a04d30
Dominik-Bogusz <dominik.bogusz1111@gmail.com>
2022-09-01 18:56:25 +0200

    Adding logic to the ProductsWindow file

--- WareCare/ProductsWindow.xaml.cs
+++ WareCare/ProductsWindow.xaml.cs
@@ -15,13 +15,53 @@ using System.Windows.Shapes;
 namespace WareCare
 {
     /// <summary>
-    /// Logika interakcji dla klasy ProductsWindow.xaml
+    /// Interaction logic for ProductsWindow.xaml
     /// </summary>
     public partial class ProductsWindow : Window
     {
+
+        /// <summary>
+        /// constructor initializes <c>ProductsWindow</c> object
+        /// </summary>
         public ProductsWindow()
         {
             InitializeComponent();
+            dgridProducts.ItemsSource = GetProducts();
+        }
+
+        private List<Product> GetProducts()
+        {
+            using (WareCareContext db = new WareCareContext(MainWindow.connectionString))
+            {
+                var products = (from p in db.Products select p).ToList();
+                return products;
+            }
+        }
+
+        private void AddProduct_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+
+        private void EditProduct_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void DeleteProduct_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void AddToAvailable_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
