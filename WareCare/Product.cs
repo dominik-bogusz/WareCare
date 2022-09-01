34c305f8cd96532b671366f794428350f18a8fce
Dominik-Bogusz <dominik.bogusz1111@gmail.com>
2022-09-01 18:12:32 +0200

    Some fixes to Product.cs file

--- WareCare/Product.cs
+++ WareCare/Product.cs
@@ -30,5 +30,47 @@ namespace WareCare
         /// int <c>BrandID</c> refers to Product BrandID in mysql db which also is a reference from foreign key from table Brands (ID)
         /// </summary>
         public int BrandID { get; set; }
+
+        /// <summary>
+        /// int <c>Quantity</c> refers to AvailableProduct Quantity in mysql db in order to bind it in a datagrid
+        /// </summary>
+        public int Quantity { get => GetQuantity(); }
+
+        /// <summary>
+        /// nullable string <c>Brand</c> refers to Brands Name in mysql db in order to bind it in a datagrid
+        /// </summary>
+        public string? Brand { get => GetBrand(); }
+
+        /// <summary>
+        /// nullable decimal <c>Price</c> refers to AvailableProduct Price in mysql db in order to bind it in a datagrid
+        /// </summary>
+        public decimal? Price { get => GetPrice(); }
+
+        private string? GetBrand()
+        {
+            using (WareCareContext db = new WareCareContext(@"Data Source=DESKTOP-VC01PS3\SQLEXPRESS;Initial Catalog=WareCare;Integrated Security=True"))
+            {
+                string brand = (from a in db.Brands where a.ID == this.BrandID select a.Name).FirstOrDefault();
+                return brand;
+            }
+        }
+
+        private int GetQuantity()
+        {
+            using (WareCareContext db = new WareCareContext(@"Data Source=DESKTOP-VC01PS3\SQLEXPRESS;Initial Catalog=WareCare;Integrated Security=True"))
+            {
+                int quantity = (from a in db.AvailableProducts where a.ProductID == this.ID select a.Quantity).FirstOrDefault();
+                return quantity;
+            }
+        }
+
+        private decimal? GetPrice()
+        {
+            using (WareCareContext db = new WareCareContext(@"Data Source=DESKTOP-VC01PS3\SQLEXPRESS;Initial Catalog=WareCare;Integrated Security=True"))
+            {
+                decimal? price = (from a in db.AvailableProducts where a.ProductID == this.ID select a.Price).FirstOrDefault();
+                return price;
+            }
+        }
     }
 }
